c928654bc544c1d986ef72b8c7e3f04ed267d2b8
Yurina Shin <yurinas@gmail.com>
2020-06-04 05:31:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Driver {
     );
   }
   passengers(){
-    return this.trips();
+    return store.trips().bind(this);
   //   return store.trips.filter(
   //     function(trip){
   //       if (trip.driverId === this.id){
